23b0de9860a985c93820c821b4b7de8a94822f1f
JeqnBat <jbpellier@noos.fr>
2020-03-15 17:41:25 +0100

    update functions.js

--- functions.js
+++ functions.js
@@ -1,18 +1,5 @@
 $(function() {
-// var xhr = new XMLHttpRequest();
-// // xhr.withCredentials = true;
-//
-// xhr.addEventListener("readystatechange", function() {
-//   if(this.readyState === 4) {
-//     console.log(this.response);
-//     console.log(this);
-//   }
-// });
-//
-// xhr.open("GET", "https://www.potterapi.com/v1/characters/5a0fa4daae5bc100213c232e?key=$2a$10$8aGIy84sCcJiJvYuK.DnHekNSW9WLzjaVqKZ3.bjKema7dXkuiF6.");
-//
-// xhr.send();
-
+// test API Call with ajax
   var settings = {
     "url": "https://www.potterapi.com/v1/characters/5a0fa4daae5bc100213c232e?key=$2a$10$8aGIy84sCcJiJvYuK.DnHekNSW9WLzjaVqKZ3.bjKema7dXkuiF6.",
     "method": "GET",
