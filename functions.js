155e408d87a29ac6a49b658deeabf5ca3da763b5
JeqnBat <jbpellier@noos.fr>
2020-03-19 16:07:51 +0100

    Function+index+css

--- functions.js
+++ functions.js
@@ -1,14 +1,24 @@
 $(function() {
 // test API Call with ajax
-  var settings = {
-    "url": "https://www.potterapi.com/v1/characters/5a0fa4daae5bc100213c232e?key=$2a$10$8aGIy84sCcJiJvYuK.DnHekNSW9WLzjaVqKZ3.bjKema7dXkuiF6.",
-    "method": "GET",
-    "timeout": 0,
-  };
+  // var settings = {
+  //   "url": "https://www.potterapi.com/v1/characters/5a0fa4daae5bc100213c232e?key=$2a$10$8aGIy84sCcJiJvYuK.DnHekNSW9WLzjaVqKZ3.bjKema7dXkuiF6.",
+  //   "method": "GET",
+  //   "timeout": 0,
+  // };
+  //
+  // $.ajax(settings).done(function (response) {
+  //   console.log(response);
+  //   console.log(response.name);
+  // });
 
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    console.log(response.name);
-  });
+
+  fetch('json.json').then(response => {
+  return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+  })
 
 });
