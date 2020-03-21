5f4e4e887cbb52036057be4d8e6c09d90769590d
JeqnBat <jbpellier@noos.fr>
2020-03-21 18:42:14 +0100

    Cleansed function/added geolocate + console.log() in index to show coordinates

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 $(function() {
-// test API Call with ajax
+
+  // __________Partie 1 : Appel de l'API des restaurants__________
+  // Méthode 1 - Ajax
   // var settings = {
   //   "url": "http://127.0.0.1/OCP7/json.json",
   //   "method": "GET",
@@ -10,7 +12,7 @@ $(function() {
   //   console.log(response);
   // });
 
-  // Méthode fetch
+  // Méthode 2 - Fetch
   let customInit =  { method: 'GET',
                       headers: {
                         'Content-Type': 'application/json'
@@ -30,19 +32,55 @@ $(function() {
       console.log(data.restaurant[0].address);
     });
 
-  // avec les fonctions fléchées
+  // Méthode 2bis - Fetch avec fonctions fléchées
   // fetch(jsonFile, {mode: 'no-cors'})
   //   .then(response => {
   //     response.json()
   //   })
   //   .then(data => {
-  //     console.log("LOL")
+  //     console.log()
   //   })
   //   .catch(err => {
-  //     console.log('KIKOO')
+  //     console.log()
   //   });
 
-
+  // Méthode 3 - avec Json.parse()
   // var myObj = JSON.parse(jsonFile);
   // document.getElementById("right").innerHTML = myObj.restaurant[0].address;
+
+  // __________Partie 2 : Appel de l'API des restaurants__________
+  function geoLoc() {
+    function sortLongLat(ip) {
+      // GeoLoc - Partie 2 - Passer l'IP du user à l'API ipstack & stocker sa latitude & longitude
+      fetch('http://api.ipstack.com/' + ip + '?access_key=c8d9b4cb9b996824a9660a661545ab41')
+        .then(function(resp) {
+          return resp.json();
+        })
+        .then(function(data) {
+          console.log(data.longitude);
+          console.log(data.latitude);
+          console.log(data);
+          let longitude = data.longitude;
+          let latitude = data.latitude;
+        });
+      }
+
+      $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+        let toto = data.indexOf('ip=')+3;
+        let tata = data.indexOf('ts=');
+        console.log(toto);
+        console.log(tata);
+        let ip = data.substring(toto, tata);
+        console.log(ip);
+        sortLongLat(ip);
+      });
+    }
+
+    geoLoc();
+
+  // réfléchit :
+  // 1. je sélectionne ip= comme point de départ et je dis
+  // tant qu'on est pas arrivé à ts, copie la string dans la variable IP
+  // ok step by step
+
 });
