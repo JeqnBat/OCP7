9f85fa9dbc668d6b7121109305ae8f8517b13e1b
JeqnBat <jbpellier@noos.fr>
2020-03-28 11:53:54 +0100

    Saving old function.js before editing it

--- functions.js
+++ functions.js
@@ -1,7 +1,8 @@
 $(function() {
+  // __________ Partie 1 : Appel de l'API des restaurants __________
+  ////////////////////////////////////////////////////////////////////////////
+  // Méthode 1 - Ajax -
 
-  // __________Partie 1 : Appel de l'API des restaurants__________
-  // Méthode 1 - Ajax
   // var settings = {
   //   "url": "http://127.0.0.1/OCP7/json.json",
   //   "method": "GET",
@@ -17,26 +18,65 @@ $(function() {
   // document.getElementById("right").innerHTML = myObj.restaurant[0].address;
 
   // utiliser typeof pour vérifier si c'est un objet
+  ////////////////////////////////////////////////////////////////////////////
 
+
+  // __________ créer une fonction init qui doit : __________
+  //            .synchroniser toutes les fonctions dans la bonne séquence
+  //            .await-async le fetch
+  //            .préparer la suite du programme
+
+  let restaurants = {} // Créer l'objet qui va accueillir les data du JSON
+  // Enchâsser l'appel API dans une fonction ASYNC pour le faire passer en SYNC et l'intégrer dans la séquence d'init
+  const init = async () => {
+    // Définition des 2 arguments de fetch()
+    let customInit =  { method: 'GET',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        mode: 'no-cors',
+                        cache: 'default'
+                      }
+
+    let jsonFile = 'http://127.0.0.1/OCP7/json.json'
+    // Création de la requête
+    let response = await fetch(jsonFile, customInit)
+    let data = await response.json()
+    let restaurants = data // on stocke le retour du JSON dans l'objet restaurants
+    console.log(data)
+    console.log(restaurants.length)
+    console.log(restaurants[0].restaurantName)
+
+    // dois-je écrire tout mon programme ici ?
+  }
+
+  init()
   // Méthode 2 - Fetch
-  let customInit =  { method: 'GET',
-                      headers: {
-                        'Content-Type': 'application/json'
-                      },
-                      mode: 'no-cors',
-                      cache: 'default'
-                    }
-
-  let jsonFile = 'http://127.0.0.1/OCP7/json.json'
-
-  fetch(jsonFile, customInit)
-    .then(function(resp) {
-      return resp.json()
-    })
-    .then(function(data) {
-      console.log(data)
-      console.log(data.restaurant[0].address)
-    });
+//   let customInit =  { method: 'GET',
+//                       headers: {
+//                         'Content-Type': 'application/json'
+//                       },
+//                       mode: 'no-cors',
+//                       cache: 'default'
+//                     }
+//
+//   let jsonFile = 'http://127.0.0.1/OCP7/json.json'
+//
+// // Ce fetch est asynchrone, OR, il doit être synchrone et s'inscrire dans la séquence d'initialisation
+//   fetch(jsonFile, customInit)
+//     .then(res => {
+//       if(res.ok) {
+//         return res.json ()
+//       } else {
+//         throw new Error('ERROR')
+//       }
+//     })
+//     .then(data => {
+//       restaurants = data // On stocke le data du JSON dans un objet
+//       console.log(restaurants.length)
+//       console.log(restaurants[0].restaurantName)
+//     })
+//     .catch(error => console.log('ERROR'))
 
   // Méthode 2bis - Fetch avec fonctions fléchées
   // fetch(jsonFile, {mode: 'no-cors'})
@@ -57,9 +97,9 @@ $(function() {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     };
-    localStorage.getItem('map', map) // reçoit l'objet map d'index.html
+    localStorage.getItem('map') // reçoit l'objet map d'index.html
     map.setCenter(pos) // update la map avec la position reçue
-    map.setZoom(15)
+    map.setZoom(16)
   }
 
   function geoLocError(err) {
@@ -74,7 +114,14 @@ $(function() {
       }
   }
 
-  geoLoc()
+
+  // __________Partie 3 : Display des restaurants du JSON sur la map__________
+
+geoLoc()
+
+// 1. récupérer le length de mon array restaurants
+// 2. créer une boucle sur la longueur de cet arret en i++
+// 3. pour chaque tour de boucle, append le DOM dans la div dédiée, avec toutes les infos du resto
 
 
 });
