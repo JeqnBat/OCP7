b4c4ddf1cd0e0bef33579f3b84c06a5b974de01e
JeqnBat <jbpellier@noos.fr>
2020-04-02 16:57:13 +0200

    Ref #5 - StreetView is working / filter function update

--- functions.js
+++ functions.js
@@ -1,21 +1,12 @@
 $(function() {
-  /* Proposition pour la STRUCTURE du programme :
-    .Une fonction async 'MASTER' qui appelle toutes les autres fonctions dans le bon ordre
-        ---- Bloc asynchrone ----
-        const master = async() {
-              await init() {}
-              await geoLoc() {}
+  // Reçoit les objets map et StreetViewPanorama d'index.html
+  localStorage.getItem('map', 'pano')
 
-              etc.
-        }
-        ---- Appel ----
-        master()
-        DOM()
-  */
   let places = []
   let allRates = []
-  localStorage.getItem('map') // Reçoit l'objet map d'index.html
-  // ----------------------------- (1) BLOC ASYNCHRONE -----------------------------
+  let allMarkers = []
+  let pos = {}
+
   // __________ Partie 1 : Appel de l'API des restaurants __________
   // Enchâsser l'appel API dans une fonction ASYNC
   const getData = async () => {
@@ -42,78 +33,22 @@ $(function() {
       // ...
     }
   }
-
   function userPosition(position) {
-    let pos = {
+    pos = {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     }
     map.setCenter(pos) // update la map avec la position reçue
     map.setZoom(16)
+    pano.setPosition(pos)
   }
-
   function geoLocError(err) {
     console.warn(`ERREUR (${err.code}): ${err.message}`)
   }
 
-  // __________ Partie 3.0 : Display des restaurants du JSON __________
-  // I. Display des infos dans le DOM
-  function processData(data) {
-    // Calcul de la moyenne(1) - déclaration des variables
-    let sum = 0
-    let averageRate = 0
-    // Display des data
-    for (let i = 0; i < data.length; i++) {
-      $('#two').append(`<div class="text" item="${i}">
-      Nom : ${data[i].restaurantName}<br>
-      Adresse : ${data[i].address}<br>
-      <p class="averageRate"></p>
-      <hr>`)
-      // Nouvelle boucle pour process l'array dans l'array
-      for (let j = 0; j < data[i].ratings.length; j++) {
-        $(`[item = ${i}]`).append(
-        `Note : ${data[i].ratings[j].stars}<br>
-        Commentaire : ${data[i].ratings[j].comment}<br>
-        </div>`)
-        // Calcul de la moyenne(2) - stockage de la somme
-        sum += data[i].ratings[j].stars
-      }
-      // Calcul de la moyenne(3) - computation & affichage
-      let denominator = data[i].ratings.length
-      let averageRate = sum / denominator
-      allRates.push(averageRate)
-      sum = 0
-      // Display de la note moyenne
-      $('.averageRate:last').append(`Note moyenne : ${averageRate}`)
-    }
-    // __________ Partie 3.1 : Création d'un filtre basé sur les notes __________
-    // stocker la valeur des deux input number
-    let minRate = $('#minRate').val()
-    let maxRate = $('#maxRate').val()
-
-    function filterByRate() {
-      for (let i = 0; i < allRates.length; i++) {
-        if (allRates[i] >= minRate && allRates[i] <= maxRate) { // factoriser cette ligne ?? (allRatesx2)
-          $(`[item = ${i}]`).css('display', '')
-        } else {
-          $(`[item = ${i}]`).css('display', 'none')
-        }
-      }
-    }
-    // Event handler : MAJ les notes en cliquant sur "Filtrer" puis compute les nouvelles notes et les renvoie
-    $('#filter').click(function() {
-      minRate = $('#minRate').val()
-      maxRate = $('#maxRate').val()
-      filterByRate()
-    })
-  } // fin de processData()
-
-  // __________ Partie 3.2 : Display des restaurants sur la Gmap __________
+  // __________ Partie 3 : Display des restaurants sur la Gmap __________
   function updateGmap(data) {
-    // 1. On stocke tous les marqueurs générés ici
-    let allMarkers = []
     let markerPNG = 'png/marker.png'
-
     // 2. On génère TOUS les marqueurs à partir de l'array 'places' qui provient du JSON
     for (let i = 0; i < data.length; i++) {
       let latLng = new google.maps.LatLng(data[i].lat, data[i].long)
@@ -129,68 +64,111 @@ $(function() {
       // On envoie tous les markers dans l'array allMarkers
       allMarkers.push(marker)
     }
-
     // 3 Gestion événementielle des markers
     function showMarkerDetails(marker) {
       let infowindow = new google.maps.InfoWindow({
-        content: 'ca viendra tkt'
+        content: marker.title
       })
       // 3.1 Onclick
       marker.addListener('click', function() {
-
         // 3.1.1 Identifier le marker cliqué dans l'array des restaurants
+        let sum = 0
+        let averageRate = 0
         let n = 0
           while (marker.title != data[n].restaurantName) {
               n++
           }
-        // 3.1.2 passer 'n' au code pour obtenir toutes les infos
-        $('#three').html(`<div class="text" item="${n}">
-        Nom : ${data[n].restaurantName}<br>
-        Adresse : ${data[n].address}<br>
-        <p class="averageRate"></p>
-        <hr>`)
-        // Nouvelle boucle pour process l'array dans l'array
-        for (let j = 0; j <data[n].ratings.length; j++) {
-          $('[item]:last').append(
-          `Note : ${data[n].ratings[j].stars}<br>
-          Commentaire : ${data[n].ratings[j].comment}<br>
-          </div>`)
-        }
-      })
+        // 3.1.2 vérifier que la fiche restaurant n'a pas déjà été APPEND dans #two
+        if ($('#two').html().indexOf(`${data[n].restaurantName}`) == -1) {
+          // passer 'n' au code pour obtenir toutes les infos
+          $('#two').append(`<div class="text" item="${data[n].restaurantName}">
+          Nom : ${data[n].restaurantName}<br>
+          Adresse : ${data[n].address}<br>
+          <p class="averageRate"></p>
+          <hr>`)
+          // Nouvelle boucle pour process l'array dans l'array
+          for (let j = 0; j < data[n].ratings.length; j++) {
+            $('[item]:last').append(
+            `Commentaire : ${data[n].ratings[j].comment}<br>
+            Note : ${data[n].ratings[j].stars}<br>
+            <br>
+            </div>`)
+            sum += data[n].ratings[j].stars
+          }
 
+          let denominator = data[n].ratings.length
+          averageRate = sum / denominator
+          $('.averageRate:last').append(`Note moyenne : ${averageRate}`)
+
+          allRates.push(averageRate, `${data[n].restaurantName}`)
+          sum = 0
+
+        } else {
+          // rajouter toggle plus tard via CSS pour montrer que le resto est déjà là
+        }
+        // StreetViewPanorama update
+        latLng = new google.maps.LatLng(data[n].lat, data[n].long)
+        pano.setPosition(latLng)
+      })
+      // 3.2 Mouseover
       marker.addListener('mouseover', function() {
         infowindow.open(marker.get('map'), marker)
       })
-
       marker.addListener('mouseout', function() {
         infowindow.close()
       })
     }
 
-    // 3. On crée une fonction qui renvoie TRUE si le marker est dans les limites visibles de la MAP
+    // 4. On crée une fonction qui renvoie TRUE si le marker est dans les limites visibles de la MAP
     function checkPos(marker) {
       return map.getBounds().contains(marker.getPosition())
     }
-    // 4. On utilise un eventHandler pour MAJ la map à chaque "dragend"
+    // 5. On utilise un eventHandler pour MAJ la map à chaque "dragend"
     google.maps.event.addListener(map, 'dragend', function() {
-      // 4.1 On boucle sur tous les marqueurs à la fin de chaque "dragend"
+      // 5.1 On boucle sur tous les marqueurs à la fin de chaque "dragend"
       for (let i = 0; i < allMarkers.length; i++) {
-        // 4.2 Si l'un des marqueurs est dans les limites de la map on l'affiche, sinon non
+        // 5.2 Si l'un des marqueurs est dans les limites de la map on l'affiche, sinon non
         if (checkPos(allMarkers[i]) == true) {
           allMarkers[i].setVisible(true)
         } else {
           allMarkers[i].setVisible(false)
         }
       }
-    }) // fin du event handler
-  } // fin de updateGmap()
+    })
+  }
+  // __________ Partie 4 : Création d'un filtre basé sur les notes __________
+  // stocker la valeur des deux input number
+  let minRate = $('#minRate').val()
+  let maxRate = $('#maxRate').val()
+
+  function filterByRate() {
+    for (let i = 0; i < allRates.length; i++) {
+      if (typeof allRates[i] === 'string' ) {
+        continue
+      }
+      if (allRates[i] >= minRate && allRates[i] <= maxRate) { // factoriser cette ligne ?? (allRatesx2)
+        $(`[item=${allRates[i+1]}]`).css('display', '')
+      } else {
+        $(`[item=${allRates[i+1]}]`).css('display', 'none')
+        // allMarkers[i].setVisible(true)
+        console.log(allMarkers[0].title)
+        console.log(allRates[i+1])
+      }
+    }
+  }
+  // Event handler : MAJ les notes en cliquant sur "Filtrer" puis compute les nouvelles notes et les renvoie
+  $('#filter').click(function() {
+    minRate = $('#minRate').val()
+    maxRate = $('#maxRate').val()
+    filterByRate()
+  })
 
   // ----------------------------- (2) APPELS -----------------------------
   const master = async () => {
     const geoLocRdy = await geoLoc()
     const dataRdy = await getData()
-    processData(places)
     updateGmap(places)
+    filterByRate()
   }
 
   master()
